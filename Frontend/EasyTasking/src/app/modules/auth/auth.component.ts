6ca2f865eef119028fd44e931b5198aa087c8312
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-05-26 14:51:05 +0530

    Added working validations

--- Frontend/EasyTasking/src/app/modules/auth/auth.component.ts
+++ Frontend/EasyTasking/src/app/modules/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormControl ,Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {CustomValidators} from '../../utils/custom-validators';
 
 @Component({
   selector: 'app-auth',
@@ -11,33 +12,53 @@ export class AuthComponent implements OnInit {
 
   isLogin = true;
   isChecked = true;
+  isSubmitting = null;
 
-  
-    registrationForm= new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl(),
-      password: new FormControl(),
-      gender: new FormControl()
-    });
+  registrationForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+    password: new FormControl('', [
+      Validators.required,
+      CustomValidators.patternValidator(/\d/, {hasNumber: true}),
+      CustomValidators.patternValidator(/[A-Z]/, {hasCapitalCase: true}),
+      CustomValidators.patternValidator(/[a-z]/, {hasSmallCase: true}),
+      Validators.minLength(8)]),
+    gender: new FormControl('', Validators.required),
+    phone: new FormControl()
+  });
 
-    loginForm= new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
-    });
-    get f(){
-      return this.registrationForm.controls;
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  get registerForm() {
+    return this.registrationForm.controls;
+  }
+
+  get authForm() {
+    return this.loginForm.controls;
+  }
+
+  submitForm(form) {
+    if (form === 'register') {
+      console.log(this.registrationForm.value);
+    } else if (form === 'auth') {
+      console.log(this.loginForm.value);
     }
-    submit(){
-      alert('hello')
-      console.log(this.registrationForm)
-      return false;
-    }
-  constructor(private router: ActivatedRoute) { }
+    this.isSubmitting = form;
+  }
+
+  constructor(private router: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     this.router.params.subscribe(params => {
-      if (params.param == 'signup') this.isLogin = false;
-      else this.isLogin = true;
+      if (params.param === 'signup') {
+        this.isLogin = false;
+      } else {
+        this.isLogin = true;
+      }
     });
   }
 
--- Frontend/EasyTasking/src/app/modules/auth/auth.module.ts
+++ Frontend/EasyTasking/src/app/modules/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
