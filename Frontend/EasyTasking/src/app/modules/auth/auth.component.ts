cf1bc249bbfa5cf1468ecbca0ef035a25da385c4
Praveen Sharma <Praveen-Sharma00>
2020-05-31 11:05:21 +0530

    Changes in Frontend

--- Frontend/EasyTasking/src/app/modules/auth/auth.component.ts
+++ Frontend/EasyTasking/src/app/modules/auth/auth.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {CustomValidators} from '../../utils/custom-validators';
-import {ServerService} from "../../service/server.service";
+import {ServerService} from '../../service/server.service';
 
 @Component({
   selector: 'app-auth',
