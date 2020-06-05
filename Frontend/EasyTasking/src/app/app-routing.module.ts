23ffd8636709750416c56e3e3ead2a78af4826c5
ajay-arya <ajayarya512@gmail.com>
2020-06-05 06:05:54 +0530

    style change

--- Frontend/EasyTasking/src/app/app-routing.module.ts
+++ Frontend/EasyTasking/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {AuthGuardService} from './service/auth-guard.service';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './service/auth-guard.service';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'authenticate',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
